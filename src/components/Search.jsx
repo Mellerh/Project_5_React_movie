bc1dc1b72017777fed7b1a9eb5cbc997ed815c11
Mellerh <103604450+Mellerh@users.noreply.github.com>
2023-07-02 23:31:50 +0300

    Search changes

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -18,6 +18,7 @@ class Search extends React.Component {
     handleSubmit = (event) => {
         if (event.key === 'Enter' && this.state.search.trim()) {
             this.props.searchFilms(this.state.search.trim());
+            
         }
     }
 
