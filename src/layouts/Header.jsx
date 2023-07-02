ca04f6dda1ca9082436c2fc7c158f1b660c43b91
Mellerh <103604450+Mellerh@users.noreply.github.com>
2023-07-02 22:24:28 +0300

    new titile

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
     return(
         <nav className="indigo darken-1">
             <div className="nav-wrapper">
-                <a href="" Name="brand-logo">React Movies</a>
+                <a href="" Name="brand-logo">React MovieS</a>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
                     <li><a href="#">@_@</a></li>
                 </ul>
